312af32757349d56da92cad5cd4e16c4b502c869
PhunkyO <99277472+PhunkyO@users.noreply.github.com>
2022-05-19 19:00:44 +0100

    header update

--- shell.h
+++ shell.h
@@ -55,5 +55,7 @@ void cd_to(data_shell *datash);
 void cd_previous(data_shell *datash);
 void cd_to_home(data_shell *datash);
 
+/* cd_shell.c */
+int cd_shell(data_shell *datash);
 
 #endif /* SHELL_H*/
